04dd4a9a837d6823d002e6dbb5657dbe6fadbdc3
Daniel <daxyt98@gmail.com>
2022-01-26 10:05:50 +0100

    Final version

--- main.cpp
+++ main.cpp
@@ -35,23 +35,26 @@ void help(){
         std::cout << "DESCRIPTION" << std::endl;   
         std::cout << "             Performs mathematical calculations with one or more matrices (must be stored in a file and is passed as a parameter)." << std::endl;   
         std::cout <<  std::endl;   
-        std::cout << "-i" << std::endl;   
+        std::cout << "   -i" << std::endl;   
         std::cout << "             To indicate that the following two parameters are the names of the files containing the matrix." << std::endl;   
         std::cout <<  std::endl;   
-        std::cout << "-o" << std::endl;   
+        std::cout << "   -o" << std::endl;   
         std::cout << "             To indicate that the following parameter is the name of the file where you want to save the result." << std::endl;   
         std::cout <<  std::endl;   
-        std::cout << "-a    (ADDITION)" << std::endl;   
+        std::cout << "   -a    (ADDITION)" << std::endl;   
         std::cout << "             To indicate that the operation to be carried out is a sum." << std::endl;   
         std::cout <<  std::endl;   
-        std::cout << "-s    (SUBTRACTION)" << std::endl;   
+        std::cout << "   -s    (SUBTRACTION)" << std::endl;   
         std::cout << "             To indicate that the operation to be carried out is a subtract." << std::endl;   
         std::cout <<  std::endl;   
-        std::cout << "-m    (MULTIPLICATION)" << std::endl;   
+        std::cout << "   -m    (MULTIPLICATION)" << std::endl;   
         std::cout << "             To indicate that the operation to be carried out is a multiplication." << std::endl;   
         std::cout <<  std::endl;   
         std::cout << "      (TRANSPOSITION)" << std::endl;   
         std::cout << "             No parameter is needed because only a default array is passed in the input." << std::endl;   
+        std::cout <<  std::endl;   
+        std::cout << "EXAMPLE" << std::endl;   
+        std::cout << "             ./matrix -i file file2 -o result -s" << std::endl;   
         std::cout <<  std::endl;   
         std::cout << "AUTHOR" << std::endl;   
         std::cout << "             Written by Daniel Alarcón Nuño." << std::endl;   
@@ -61,10 +64,6 @@ void help(){
         std::cout <<  std::endl;   
         std::cout <<  std::endl;   
         std::cout <<  std::endl;   
-
-
-
-
 }
 
 /**
@@ -134,7 +133,6 @@ int main(int argc, char* argv[]){
 
         if(strcmp(argv[6], "-a") == 0){
             if(firstMatrix.size() == secondMatrix.size() && firstMatrix[0].size() == secondMatrix[0].size()){
-
                 solution = addition(firstMatrix, secondMatrix); 
                 printMatrix(solution);
                 print_matrix(fileOutput,solution);
@@ -167,52 +165,5 @@ int main(int argc, char* argv[]){
     }
     std::vector<std::string> cmdLineArgs(argv, argv+argc);
 
-
-
     std::vector<std::vector<double>> aux; 
-/*
-    bool loop = true;
-    while (loop){
-        
-        if (x == 0){
-            loop = false;
-        }
-        else if (x == 1){
-            if(firstMatrix.size() == secondMatrix.size() && firstMatrix[0].size() == secondMatrix[0].size()){
-                aux = addition(firstMatrix, secondMatrix); 
-                printMatrix(aux);
-            }else{
-                std::cout << "The matrices have to be of the same size." << std::endl;                
-            }
-        }
-        else if (x == 2){
-            if(firstMatrix.size() == secondMatrix.size() && firstMatrix[0].size() == secondMatrix[0].size()){
-                aux = subtraction(firstMatrix, secondMatrix); 
-                printMatrix(aux);
-            }else{
-                std::cout << "The matrices have to be of the same size." << std::endl;                
-            }
-        }
-        else if(x == 3){
-            if(firstMatrix[0].size() == secondMatrix.size()){
-                aux = multiplication(firstMatrix, secondMatrix); 
-                printMatrix(aux);
-            }
-        }
-        else{
-            std::cout << "Which matrix do you want to transpose? (1-2) " << std::endl;
-            int option;
-            cin >> option;
-            if(option == 1){
-              aux = transposition(firstMatrix);
-              printMatrix(aux);
-            }else{
-            aux = transposition(secondMatrix);
-            printMatrix(aux);
-            }
-        }
-       
-        }
-    }   
-    */    
 }
